dec4cc55b0819d5b80b90de1e2c48cb575b5d976
Johan "Zephyyrr" Fogelström <the@excellent.ninja>
2021-01-31 19:17:37 +0100

    Small fixes in Docker flow.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+
 
 if [ -x "$(command -v gunicorn)" ]; then
     gunicorn --bind 0.0.0.0:8000 wsgi
 else
     echo "Gunicorn not installed: Using built-in server."
+    python manage.py collectstatic --noinput
     watchmedo shell-command --patterns="*.css" -R -c "python manage.py collectstatic --noinput" &
     python manage.py runserver 0.0.0.0:8000
 fi
